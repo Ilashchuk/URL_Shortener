6c55453c5b15b8e0ea1c9396d4ce8275490c62d9
Bohdan <44bohdan44@gmail.com>
2022-10-01 23:54:37 +0300

    Controllers and views for users and urls has been added by scafolding

---
+++ URL_Shortener/Controllers/UrlsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using URL_Shortener.Data;
+using URL_Shortener.Models;
+
+namespace URL_Shortener.Controllers
+{
+    public class UrlsController : Controller
+    {
+        private readonly URL_Shortener_Context _context;
+
+        public UrlsController(URL_Shortener_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Urls
+        public async Task<IActionResult> Index()
+        {
+            var uRL_Shortener_Context = _context.Urls.Include(u => u.User);
+            return View(await uRL_Shortener_Context.ToListAsync());
+        }
+
+        // GET: Urls/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Urls == null)
+            {
+                return NotFound();
+            }
+
+            var url = await _context.Urls
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            return View(url);
+        }
+
+        // GET: Urls/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Urls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Link,ShortLink,UserId,Date")] Url url)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(url);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", url.UserId);
+            return View(url);
+        }
+
+        // GET: Urls/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Urls == null)
+            {
+                return NotFound();
+            }
+
+            var url = await _context.Urls.FindAsync(id);
+            if (url == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", url.UserId);
+            return View(url);
+        }
+
+        // POST: Urls/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Link,ShortLink,UserId,Date")] Url url)
+        {
+            if (id != url.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(url);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UrlExists(url.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Email", url.UserId);
+            return View(url);
+        }
+
+        // GET: Urls/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Urls == null)
+            {
+                return NotFound();
+            }
+
+            var url = await _context.Urls
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (url == null)
+            {
+                return NotFound();
+            }
+
+            return View(url);
+        }
+
+        // POST: Urls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Urls == null)
+            {
+                return Problem("Entity set 'URL_Shortener_Context.Urls'  is null.");
+            }
+            var url = await _context.Urls.FindAsync(id);
+            if (url != null)
+            {
+                _context.Urls.Remove(url);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UrlExists(int id)
+        {
+          return _context.Urls.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ URL_Shortener/Migrations/20221001203707_URL_Shortener_Migration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URL_Shortener.Migrations
+{
+    public partial class URL_Shortener_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Urls_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_UserId",
+                table: "Urls",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
